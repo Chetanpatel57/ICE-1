40a07a285aab8d472fb4902797103431acc7647e
Chetanpatel57 <40812586+Chetanpatel57@users.noreply.github.com>
2023-01-22 16:03:07 -0500

    Added game object and protocol

---
+++ Lab_1/Lab_1/Core/GameObject.swift
@@ -0,0 +1,61 @@
+//
+//  GameObject.swift
+//  Lab_1
+//
+//  Created by Chetan Patel on 2023-01-22.
+//
+
+import GameplayKit
+import SpriteKit
+
+class GameObject : SKSpriteNode, GameProtocol{
+    func Start() {
+        
+    }
+    
+    func Update() {
+        
+    }
+    
+    func CheckBounds() {
+        
+    }
+    
+    func Reset() {
+        
+    }
+    
+    var horizontalSpeed : CGFloat?
+    var verticalSpeed : CGFloat?
+    var width : CGFloat?
+    var height : CGFloat?
+    var halfWidth : CGFloat?
+    var halfheight : CGFloat?
+    var scale : CGFloat?
+    var isColliding : Bool?
+    var randomSource : GKARC4RandomSource?
+    var randomDist : GKRandomDistribution?
+    
+    init(imageString : String, initialScale: CGFloat)
+    {
+        let texture = SKTexture(imageNamed: imageString)
+        let color = UIColor.clear
+        super.init(texture: texture, color: color, size: texture.size())
+        
+        scale = initialScale
+        setScale(scale!)
+        width = texture.size().width*scale!
+        height = texture.size().height * scale!
+        halfWidth = width! * 0.5
+        halfheight = height! * 0.5
+        isColliding = false
+        name = imageString
+        randomSource = GKARC4RandomSource()
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        fatalError("init(coder:)has not been impelented")
+    }
+}
